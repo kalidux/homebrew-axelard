14781f3b9039f8906990310b02a64c13202fe3ef
kalid <kalid@axelar.network>
2023-03-06 07:05:22 +0000

    create formula

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -1,7 +1,7 @@
 class Axelard < Formula
   desc Axelard binary for darwin
   homepage https://axelar.network
-  url url https://github.com/axelarnetwork/axelar-core/releases/download/v0.32.2/axelard-darwin-amd64-v0.32.2.zip
+  url https://github.com/axelarnetwork/axelar-core/releases/download/v0.32.2/axelard-darwin-amd64-v0.32.2.zip
   version 0.32.2
   sha256 2303ed69ee0486814e09554449292b0b3e50f61e57248143c0d24f349df33f1d
 
