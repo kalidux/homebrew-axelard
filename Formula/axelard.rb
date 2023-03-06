4257f5d702f9d97a9f0f2b87dd1f09bdce9d5774
Kalidux <kalidux@gmail.com>
2023-03-06 21:02:17 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -2,12 +2,8 @@ class Axelard < Formula
   desc "The Axelar Network Core Daemon"
   homepage "https://github.com/axelarnetwork/axelar-core"
   license "Apache-2.0"
-  
-  # Allow the user to specify the version
-  # Example usage: brew install axelard --version=0.32.2
-  # Default version is 0.32.2
-  version ENV.fetch("HOMEBREW_FORMULA_VERSION", "0.32.2")
-  
+  version "0.32.2"
+
   if Hardware::CPU.arm?
     url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-arm64-v#{version}.zip"
   else
@@ -23,22 +19,20 @@ class Axelard < Formula
 
     # Download the SHA256 checksum for the release
     sha256_url = "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.sha256"
-    sha256 = curl_output(sha256_url).split.first
+    sha256 = `curl -sSL #{sha256_url} | cut -d " " -f 1`
     raise "Failed to retrieve SHA256 checksum" if sha256.empty?
 
+    # Download the release archive
+    system "curl -sSL #{url} -o axelard.zip"
     # Verify the SHA256 checksum of the downloaded release
-    file_name = Hardware::CPU.arm? ? "axelard-darwin-arm64-v#{version}" : "axelard-darwin-amd64-v#{version}"
-    system "echo \"#{sha256}  #{file_name}.zip\" | shasum -a 256 --check"
+    system "echo \"#{sha256}  axelard.zip\" | shasum -a 256 --check"
+    # Extract the archive
+    system "unzip -q axelard.zip"
+    # Install the binary
+    bin.install "axelard-darwin-amd64-v#{version}" => "axelard"
   end
 
   test do
     system "#{bin}/axelard", "version"
-  end
-
-  private
-
-  def curl_output(url)
-    curl_args = ["-sSL", url]
-    IO.popen(["curl", *curl_args], &:read)
   end
 end
