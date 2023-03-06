84285963bfa8944d3516c178c8d8d7e9c3d67f93
Kalidux <kalidux@gmail.com>
2023-03-06 15:45:20 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -2,7 +2,7 @@ class Axelard < Formula
   desc "Axelar Core - A decentralized interoperability network for cross-chain communication"
   homepage "https://github.com/axelarnetwork/axelar-core"
   url "https://github.com/axelarnetwork/axelar-core/releases/download/v0.32.2/axelard-darwin-amd64-v0.32.2"
-  sha256 "28b0fbfc838e5c49ca0451c2d2d40a0e0cfe4409f1a2d338e67fb0a92a0b69c7"
+  sha256 "cac744649ea01b223a04d8916b6413f3fb504cc45585cade4f511e35fcc10f40"
   license "Apache-2.0"
 
   def install
