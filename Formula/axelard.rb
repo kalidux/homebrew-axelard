4ecdf8f6d7c89c82b3decbeef5b9716876764b39
Kalidux <kalidux@gmail.com>
2023-03-06 22:07:01 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -15,25 +15,39 @@ class Axelard < Formula
 
   if Hardware::CPU.arm?
     url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-arm64-v#{version}.zip"
-    sha256_arm64 "49d9f9c8c99aa61dce81a47db3fc5f7fc32ed634e1bebd5d5f06cb7aa5b98cf5"
   else
     url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.zip"
-    sha256_amd64 "2303ed69ee0486814e09554449292b0b3e50f61e57248143c0d24f349df33f1d"
+  end
+
+  if Hardware::CPU.arm?
+    sha256_arm64 = "7c9cf071172c5838d4012b1c052f23247d3c242fc8fb1fc97c9b15abdaa931e5"
+    sha256 "#{sha256_arm64}--axelard-darwin-arm64-v#{version}.zip"
+  else
+    sha256_amd64 = "26c37d149a2142d2d68fcef583723c15cd05dcd49dd7dd98a15f70807f0b7a89"
+    sha256 "#{sha256_amd64}--axelard-darwin-amd64-v#{version}.zip"
   end
 
   def install
     if Hardware::CPU.arm?
       bin.install "axelard-darwin-arm64-v#{version}" => "axelard"
-      expected_sha256 = sha256_arm64
     else
       bin.install "axelard-darwin-amd64-v#{version}" => "axelard"
-      expected_sha256 = sha256_amd64
     end
 
+    # Download the SHA256 checksum for the release
+    sha256_url = "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.sha256"
+    sha256_output = `curl -sSL #{sha256_url}`
+
+    # Extract the SHA256 checksum from the output
+    sha256 = sha256_output[/(\h{64})/, 1]
+    raise "Failed to retrieve SHA256 checksum" if sha256.nil?
+
     # Verify the SHA256 checksum of the downloaded release
-    output = shell_output("shasum -a 256 #{bin}/axelard")
-    actual_sha256 = output.split.first
-    raise "SHA256 mismatch: expected #{expected_sha256} but got #{actual_sha256}" if expected_sha256 != actual_sha256
+    if Hardware::CPU.arm?
+      system "shasum -a 256 -c <<<\"#{sha256}  #{bin}/axelard\""
+    else
+      system "echo \"#{sha256}  #{bin}/axelard\" | shasum -a 256 -c"
+    end
   end
 
   test do
