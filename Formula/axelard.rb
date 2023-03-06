c4192ec998d416e16751a19997feefd7ebfc2297
Kalidux <kalidux@gmail.com>
2023-03-06 14:06:38 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -6,7 +6,7 @@ class Axelard < Formula
             sha256 "cac744649ea01b223a04d8916b6413f3fb504cc45585cade4f511e35fcc10f40"    
          
   def install
-    bin.install "axelard"
+    bin.install "axelard-darwin-amd64-v0.32.2"
   end
             
 end
