4344aec8bb4988e7b4fc3d51e3e7f39eb416c191
Kalidux <kalidux@gmail.com>
2023-03-06 21:18:18 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -2,38 +2,53 @@ class Axelard < Formula
   desc "The Axelar Network Core Daemon"
   homepage "https://github.com/axelarnetwork/axelar-core"
   license "Apache-2.0"
-  
-  # User can specify the version
-  option "with-version=", "Specify the version of axelard to install"
 
-  # Set the default version to the latest release
-  latest_release = `curl --silent "https://api.github.com/repos/axelarnetwork/axelar-core/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")'`
-  version "#{latest_release}"
-  
-  # Override the version if the user specified one
-  if build.with? "version"
-    version "#{build.with("version")}"
-  end
+  option "without-version", "Install the latest version"
 
-  if Hardware::CPU.arm?
-    url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-arm64-v#{version}.zip"
+  if build.without?("version")
+    url "https://github.com/axelarnetwork/axelar-core/releases/latest/download/axelard-darwin-amd64-latest.zip"
+    version "latest"
   else
-    url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.zip"
+    version "0.32.2"
+    if Hardware::CPU.arm?
+      url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-arm64-v#{version}.zip"
+    else
+      url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.zip"
+    end
   end
-  
-  # Use the 'get_sha256_checksum' function to retrieve the SHA256 checksum
-  sha256 get_sha256_checksum(url)
 
   def install
+    if build.without?("version")
+      release_page = "https://github.com/axelarnetwork/axelar-core/releases/latest"
+      release_info = `curl -sSL #{release_page}`
+      url = release_info.match(%r{href="(https://github.com/axelarnetwork/axelar-core/releases/download/[^"]+/axelard-darwin-amd64-[^"]+.zip)"})[1]
+      version = release_info.match(%r{href="https://github.com/axelarnetwork/axelar-core/releases/tag/([^"]+)"})[1]
+
+      if Hardware::CPU.arm?
+        url.sub!(/darwin-amd64/, "darwin-arm64")
+      end
+
+      odie "Failed to extract download URL" if url.nil?
+      odie "Failed to extract version" if version.nil?
+
+      sha256_url = url + ".sha256"
+      sha256 = `curl -sSL #{sha256_url} | cut -d " " -f 1`
+      raise "Failed to retrieve SHA256 checksum" if sha256.empty?
+    else
+      sha256_url = "#{url}.sha256"
+      sha256 = `curl -sSL #{sha256_url} | cut -d " " -f 1`
+      raise "Failed to retrieve SHA256 checksum" if sha256.empty?
+
+      file_name = url.split("/")[-1]
+      system "curl -sSL #{url} -o #{file_name}"
+      system "shasum -a 256 -c <<<\"#{sha256}  #{file_name}\""
+    end
+
     if Hardware::CPU.arm?
       bin.install "axelard-darwin-arm64-v#{version}" => "axelard"
     else
       bin.install "axelard-darwin-amd64-v#{version}" => "axelard"
     end
-    
-    # Verify the SHA256 checksum of the downloaded release
-    file_name = Hardware::CPU.arm? ? "axelard-darwin-arm64-v#{version}" : "axelard-darwin-amd64-v#{version}"
-    system "echo \"#{sha256}  #{file_name}.zip\" | shasum -a 256 --check"
   end
 
   test do
