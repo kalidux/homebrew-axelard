3517f2546d3c67d75faab64b0f72ce625e5c8903
Kalidux <kalidux@gmail.com>
2023-03-06 15:42:58 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -1,17 +1,15 @@
 class Axelard < Formula
-            desc "axelard binary for darwin"
-            homepage "https://axelar.network"
-            url "https://github.com/axelarnetwork/axelar-core/releases/download/v0.32.2/axelard-darwin-amd64-v0.32.2"
-            version "0.32.2"
-            sha256 "cac744649ea01b223a04d8916b6413f3fb504cc45585cade4f511e35fcc10f40"  
-            
-            resource "binary" do
-              url 'https://github.com/axelarnetwork/axelar-core/releases/download/v0.32.2/axelard-darwin-amd64-v0.32.2'
-              sha256 "cac744649ea01b223a04d8916b6413f3fb504cc45585cade4f511e35fcc10f40"
-            end
-         
+  desc "Axelar Core - A decentralized interoperability network for cross-chain communication"
+  homepage "https://github.com/axelarnetwork/axelar-core"
+  url "https://github.com/axelarnetwork/axelar-core/releases/download/v0.32.2/axelard-darwin-amd64-v0.32.2"
+  sha256 "28b0fbfc838e5c49ca0451c2d2d40a0e0cfe4409f1a2d338e67fb0a92a0b69c7"
+  license "Apache-2.0"
+
   def install
-    resource("binary").stage { bin.install "axelard" }
+    bin.install "axelard-darwin-amd64-v0.32.2" => "axelard"
   end
-            
+
+  test do
+    system "#{bin}/axelard", "version"
+  end
 end
