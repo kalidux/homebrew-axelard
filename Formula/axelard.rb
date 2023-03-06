1641219537eae3542f90c34fd3a2cf3bca357cac
Kalidux <kalidux@gmail.com>
2023-03-06 21:28:57 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -1,41 +1,23 @@
 class Axelard < Formula
-  desc "The Axelar Network Core Daemon"
-  homepage "https://github.com/axelarnetwork/axelar-core"
-  license "Apache-2.0"
+  desc "Official Axelar Network command-line interface (CLI)"
+  homepage "https://axelar.network"
+  url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.zip"
+  sha256 ""
+  version "0.32.2"
 
-  # Allow the user to specify a version
-  option "with-version=", "Specify the version of axelard to install"
-
-  # Default to the latest version if none is specified
   if build.with? "version"
-    version "#{build.with("version")}"
+    version = build.with?("version") ? build.with("version") : version
+    url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.zip"
+    shasum_output = `curl -sSL https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.zip.sha256`
+    sha256 shasum_output.split(" ").first
   else
-    version "0.32.2"
+    sha256 "2303ed69ee0486814e09554449292b0b3e50f61e57248143c0d24f349df33f1d"
   end
 
-  if Hardware::CPU.arm?
-    url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-arm64-v#{version}.zip"
-  else
-    url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.zip"
-  end
+  bottle :unneeded
 
   def install
-    if Hardware::CPU.arm?
-      bin.install "axelard-darwin-arm64-v#{version}" => "axelard"
-    else
-      bin.install "axelard-darwin-amd64-v#{version}" => "axelard"
-    end
-
-    # Download the SHA256 checksum for the release
-    sha256_url = "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.sha256"
-    sha256_output = `curl -sSL #{sha256_url}`
-
-    # Extract the SHA256 checksum from the output
-    sha256 = sha256_output[/(\h{64})/, 1]
-    raise "Failed to retrieve SHA256 checksum" if sha256.nil?
-
-    # Verify the SHA256 checksum of the downloaded release
-    system "shasum -a 256 -c <<<\"#{sha256}  #{bin}/axelard\""
+    bin.install "axelard"
   end
 
   test do
