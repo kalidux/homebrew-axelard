a2ea63d4a015e032f56b6ddb2a1c81648902f92b
Kalidux <kalidux@gmail.com>
2023-03-06 12:29:36 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -6,9 +6,16 @@ class Axelard < Formula
             homepage "https://axelar.network"
             url "https://github.com/axelarnetwork/axelar-core/releases/download/v0.32.2/axelard-darwin-amd64-v0.32.2"
             version "0.32.2"
-            sha256 "cac744649ea01b223a04d8916b6413f3fb504cc45585cade4f511e35fcc10f40"
-
+            sha256 "cac744649ea01b223a04d8916b6413f3fb504cc45585cade4f511e35fcc10f40"    
+         
   def install
-    bin.install "axelard-darwin-amd64-v0.32.2"
+    resource("axelard-darwin-amd64-v0.32.2").stage { bin.install "axelard" }
+    chmod 0755, "#{bin}/axelard"
   end
+
+  test do
+    system "#{bin}/axelard", "--version"
+  end
+            
+            
 end
