f4ece7069a9e339cc0d6379d64ba911d2c7ca7be
Kalidux <kalidux@gmail.com>
2023-03-06 20:41:40 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -4,28 +4,36 @@ class Axelard < Formula
   license "Apache-2.0"
   version "0.32.2"
 
+def install
   if Hardware::CPU.arm?
     url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-arm64-v#{version}.zip"
   else
     url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.zip"
   end
 
-  def install
-    if Hardware::CPU.arm?
-      bin.install "axelard-darwin-arm64-v#{version}" => "axelard"
-    else
-      bin.install "axelard-darwin-amd64-v#{version}" => "axelard"
-    end
+  # Download the SHA256 checksum for the release
+  sha256_url = "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.sha256"
+  sha256 = curl_output(sha256_url).split(" ").first
+  raise "Failed to retrieve SHA256 checksum" if sha256.empty?
 
-    # Download the SHA256 checksum for the release
-    sha256_url = "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.sha256"
-    sha256 = `curl -sSL #{sha256_url} | cut -d " " -f 1`
-    raise "Failed to retrieve SHA256 checksum" if sha256.empty?
+  # Verify the SHA256 checksum of the downloaded release
+  file_name = Hardware::CPU.arm? ? "axelard-darwin-arm64-v#{version}" : "axelard-darwin-amd64-v#{version}"
+  system "echo \"#{sha256}  #{file_name}.zip\" | shasum -a 256 --check"
 
-    # Verify the SHA256 checksum of the downloaded release
-    file_name = Hardware::CPU.arm? ? "axelard-darwin-arm64-v#{version}" : "axelard-darwin-amd64-v#{version}"
-    system "echo \"#{sha256}  #{file_name}.zip\" | shasum -a 256 --check"
+  if Hardware::CPU.arm?
+    bin.install "axelard-darwin-arm64-v#{version}" => "axelard"
+  else
+    bin.install "axelard-darwin-amd64-v#{version}" => "axelard"
   end
+end
+
+def curl_output(url)
+  output = `curl -sSL #{url}`
+  raise "Failed to fetch #{url}" if $?.exitstatus != 0
+  output
+end
+
+
 
   test do
     system "#{bin}/axelard", "version"
