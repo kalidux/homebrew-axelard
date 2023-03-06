9222734c54604d4b8e982343d93bee2d95637ea7
Kalidux <kalidux@gmail.com>
2023-03-06 21:07:35 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -2,7 +2,9 @@ class Axelard < Formula
   desc "The Axelar Network Core Daemon"
   homepage "https://github.com/axelarnetwork/axelar-core"
   license "Apache-2.0"
-  version "0.32.2"
+  
+  # Allow user to input version
+  version = ARGV.value("version") || "0.32.2"
 
   if Hardware::CPU.arm?
     url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-arm64-v#{version}.zip"
@@ -22,14 +24,9 @@ class Axelard < Formula
     sha256 = `curl -sSL #{sha256_url} | cut -d " " -f 1`
     raise "Failed to retrieve SHA256 checksum" if sha256.empty?
 
-    # Download the release archive
-    system "curl -sSL #{url} -o axelard.zip"
     # Verify the SHA256 checksum of the downloaded release
-    system "echo \"#{sha256}  axelard.zip\" | shasum -a 256 --check"
-    # Extract the archive
-    system "unzip -q axelard.zip"
-    # Install the binary
-    bin.install "axelard-darwin-amd64-v#{version}" => "axelard"
+    file_name = Hardware::CPU.arm? ? "axelard-darwin-arm64-v#{version}" : "axelard-darwin-amd64-v#{version}"
+    system "shasum -a 256 -c <(echo '#{sha256}  #{file_name}.zip')"
   end
 
   test do
