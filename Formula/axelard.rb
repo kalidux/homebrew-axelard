4caf20e70de34aafc3dc9a344dbf9b9ae5be0bc7
Kalidux <kalidux@gmail.com>
2023-03-06 16:38:56 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -1,12 +1,36 @@
 class Axelard < Formula
   desc "Axelar Core - A decentralized interoperability network for cross-chain communication"
   homepage "https://github.com/axelarnetwork/axelar-core"
-  url "https://github.com/axelarnetwork/axelar-core/releases/download/v0.32.2/axelard-darwin-amd64-v0.32.2"
-  sha256 "cac744649ea01b223a04d8916b6413f3fb504cc45585cade4f511e35fcc10f40"
   license "Apache-2.0"
-
+  
+  version "0.32.2"
+  url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}"
+  sha256 "28b0fbfc838e5c49ca0451c2d2d40a0e0cfe4409f1a2d338e67fb0a92a0b69c7"
+  
+  # Generate all possible versions using bash script
+  bash_script = <<~EOS
+    versions=($(curl -s https://api.github.com/repos/axelarnetwork/axelar-core/releases/latest | grep "tag_name" | awk '{print $2}' | sed 's/"//g' | sed 's/,//g'))
+    for version in "${versions[@]}"; do
+      url="https://github.com/axelarnetwork/axelar-core/releases/download/${version}/axelard-darwin-amd64-${version}"
+      sha256=$(curl -sL "${url}" | shasum -a 256 | awk '{print $1}')
+      echo "  bottle do"
+      echo "    root_url 'https://github.com/axelarnetwork/axelar-core/releases/download/${version}'"
+      echo "    sha256 '${sha256}'"
+      echo "    url '${url}'"
+      echo "  end"
+    done
+  EOS
+  
+  # Parse the bash script output and append it to the formula
+  bottle do
+    root_url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}"
+    sha256 "28b0fbfc838e5c49ca0451c2d2d40a0e0cfe4409f1a2d338e67fb0a92a0b69c7"
+    url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}"
+  end
+  eval `#{bash_script}`
+  
   def install
-    bin.install "axelard-darwin-amd64-v0.32.2" => "axelard"
+    bin.install "axelard-darwin-amd64-v#{version}" => "axelard"
   end
 
   test do
