f13af69875b3fbc5dccace07bc3c627c94fcb62d
Kalidux <kalidux@gmail.com>
2023-03-07 12:31:12 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -2,7 +2,7 @@ class Axelard < Formula
   desc "The Axelar Network Core binary"
   homepage "https://github.com/axelarnetwork/axelar-core"
   license "Apache-2.0"
-  
+
   # Define the available versions and platforms
   VERSIONS = {
     "0.32.0" => {
@@ -14,7 +14,7 @@ class Axelard < Formula
       arm64: "ebc8e3f9c9a0c4d4f4b41c038dc690ebf6af30e6df1de6e3fb6f3d8c8b6f0bc6"
     }
   }
-  
+
   # Determine the platform and URL based on the system architecture
   if Hardware::CPU.arm?
     platform = :arm64
@@ -25,20 +25,20 @@ class Axelard < Formula
     platform = :amd64
     puts "Intel architecture detected"
   end
-  
+
   # Select the latest version if none is specified
   latest_version = VERSIONS.keys.last
-  version = ARGV.value("version") || latest_version
+  version = ARGV[0] || latest_version
   puts "Selected version: #{version}"
-  
+
   # Retrieve the SHA256 hash for the selected version and platform
   sha256 = VERSIONS[version][platform]
   raise "Error retrieving SHA256 hash for Axelard version #{version}" unless sha256
-  
+
   # Define the URL based on the selected version and platform
   url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-#{platform}-v#{version}.zip"
   sha256 sha256
-  
+
   # Install the binary
   def install
     if Hardware::CPU.arm?
