567c6577a718c0a1fac00e4b028ba837b81c4730
Kalidux <kalidux@gmail.com>
2023-03-07 11:27:54 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -3,7 +3,7 @@ class Axelard < Formula
   homepage "https://github.com/axelarnetwork/axelar-core"
   license "Apache-2.0"
   
-  version "0.31.2"
+  version "0.32.2"
   if Hardware::CPU.arm?
     url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-arm64-v#{version}.zip"
     sha256 "ae0d01190b8f6c2d6b37a6a291f6aa8c5f2d5eb5c5e5e5f8ed5c5d85b95bb43a"
