7f9ea474cf6ab28a8416083f2fef31b8429b53b7
Kalidux <kalidux@gmail.com>
2023-03-07 12:12:50 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -2,48 +2,53 @@ class Axelard < Formula
   desc "The Axelar Network Core binary"
   homepage "https://github.com/axelarnetwork/axelar-core"
   license "Apache-2.0"
-
-  versions = ["0.32.0", "0.32.2"]
-
-  versions.each do |version|
-    if Hardware::CPU.arm?
-      url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-arm64-v#{version}.zip"
-      sha256_url = "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-arm64-v#{version}.zip.sha256"
-    else
-      url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.zip"
-      sha256_url = "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.zip.sha256"
-    end
-
-    version version
-
-    if Hardware::CPU.arm?
-      puts "ARM architecture detected"
-      puts "CPU architecture: #{Hardware::CPU.arch}"
-      puts "Is 64-bit: #{Hardware::CPU.is_64_bit?}"
-    else
-      puts "Intel architecture detected"
-    end
-
-    sha256 = ""
-
-    begin
-      sha256 = open(sha256_url).read.split(" ").first
-    rescue
-      odie "Error retrieving SHA256 hash for Axelard version #{version}"
-    end
-
-    puts "SHA256: #{sha256}"
-
-    # Install the binary
+  
+  # Define the available versions and platforms
+  VERSIONS = {
+    "0.32.0" => {
+      amd64: "2303ed69ee0486814e09554449292b0b3e50f61e57248143c0d24f349df33f1d",
+      arm64: "e152f8d87d097f9c790a07fa4e9dcc4359b0b71925146edf62ffd7d7ea857159"
+    },
+    "0.32.2" => {
+      amd64: "a2abc8da15710bae0e77571dece93a8d367b1e416da193477211ed9330cdbdf4",
+      arm64: "ebc8e3f9c9a0c4d4f4b41c038dc690ebf6af30e6df1de6e3fb6f3d8c8b6f0bc6"
+    }
+  }
+  
+  # Determine the platform and URL based on the system architecture
+  if Hardware::CPU.arm?
+    platform = :arm64
+    puts "ARM architecture detected"
+    puts "CPU architecture: #{Hardware::CPU.arch}"
+    puts "Is 64-bit: #{Hardware::CPU.is_64_bit?}"
+  else
+    platform = :amd64
+    puts "Intel architecture detected"
+  end
+  
+  # Select the latest version if none is specified
+  latest_version = VERSIONS.keys.last
+  version = ARGV.value("version") || latest_version
+  puts "Selected version: #{version}"
+  
+  # Retrieve the SHA256 hash for the selected version and platform
+  sha256 = VERSIONS[version][platform]
+  raise "Error retrieving SHA256 hash for Axelard version #{version}" unless sha256
+  
+  # Define the URL based on the selected version and platform
+  url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-#{platform}-v#{version}.zip"
+  sha256 sha256
+  
+  # Install the binary
+  def install
     if Hardware::CPU.arm?
       bin.install "axelard-darwin-arm64-v#{version}" => "axelard"
     else
       bin.install "axelard-darwin-amd64-v#{version}" => "axelard"
     end
+  end
 
-    # Verify the installation
-    test do
-      system "#{bin}/axelard", "version"
-    end
+  test do
+    system "#{bin}/axelard", "version"
   end
 end
