8cc0c341b40373295d6e8e5acdffe5eeae176079
Kalidux <kalidux@gmail.com>
2023-03-07 13:34:24 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -10,7 +10,7 @@ class Axelard < Formula
       arm64: "e152f8d87d097f9c790a07fa4e9dcc4359b0b71925146edf62ffd7d7ea857159",
     },
     "0.32.2" => {
-      amd64: "a2abc8da15710bae0e77571dece93a8d367b1e416da193477211ed9330cdbdf4",
+      amd64: "2303ed69ee0486814e09554449292b0b3e50f61e57248143c0d24f349df33f1d",
       arm64: "ebc8e3f9c9a0c4d4f4b41c038dc690ebf6af30e6df1de6e3fb6f3d8c8b6f0bc6",
     },
   }
