5b3d1794dc63e8dceb6fbb0a19445e4ee3bd6a4d
Kalidux <kalidux@gmail.com>
2023-03-07 11:50:24 +0400

    Update axelard.rb

--- Formula/axelard.rb
+++ Formula/axelard.rb
@@ -15,6 +15,19 @@ class Axelard < Formula
     url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.zip"
     sha256 "2303ed69ee0486814e09554449292b0b3e50f61e57248143c0d24f349df33f1d"
   end
+  
+    version "0.32.0"
+  if Hardware::CPU.arm?
+    puts "ARM architecture detected"
+    puts "CPU architecture: #{Hardware::CPU.arch}"
+    puts "Is 64-bit: #{Hardware::CPU.is_64_bit?}"
+    url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-arm64-v#{version}.zip"
+    sha256 "e152f8d87d097f9c790a07fa4e9dcc4359b0b71925146edf62ffd7d7ea857159"
+  else
+    puts "Intel architecture detected"
+    url "https://github.com/axelarnetwork/axelar-core/releases/download/v#{version}/axelard-darwin-amd64-v#{version}.zip"
+    sha256 "4993e2ba3937d372fd518ed6419ef8d9d5c12bd0375a10d5ac4b0a7750647a90"
+  end
 
   # Install the binary
   def install
